b71cbc861a4b9f28c22998be9dbfe728720cb6c1
Ryan Birmingham <birm@rbirm.us>
2017-02-17 16:33:49 -0500

    fin class, add test

--- soundimate.js
+++ soundimate.js
@@ -6,18 +6,25 @@
 class Soundimate{
     constuctor(theme){
         //validate the theme from the list
-        this.themes = themes = ['cafe', 'fluid', 'mechanical', 'minimalist', 'paper', 'wood'];
+        this.themes = ['cafe', 'fluid', 'mechanical', 'minimalist', 'paper', 'wood'];
+        this.noises = ['error', 'success', 'minor', 'major', 'soft'];
         if (!(this.themes.includes(theme))){
             throw new Error("Unrecognized theme used for Soundimate.")
         }
         this.theme = theme;
         }
     }
-    //sounds
 
-    // Error
-    // Success
-    // Minor
-    // Major
-    // Soft
+    //sounds
+    /** play a sound in the theme
+    * @param {String} noise - the noise type to play on the theme.
+    */
+    play(noise){
+      if (!(this.noises.includes(noise.toLowerCase()))){
+          throw new Error("Unrecognized noise type used for Soundimate.play().")
+      }
+      var filename = 'sounds/' + this.theme.toLowerCase() + '/' + noise.toLowerCase();
+      var sound= new Audio(filename);
+      sound.play();
+    }
 }
